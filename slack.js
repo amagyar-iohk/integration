7a9f6444cd922dd7f794270d9f3e749b2ce906e8
Allain Magyar <allain.magyar@iohk.io>
2024-08-21 23:57:53 -0300

    fix: undo changes

--- slack.js
+++ slack.js
@@ -1,6 +1,9 @@
-await fetch(process.env.SLACK_WEBHOOK, {
-    method: "POST",
-    body: JSON.stringify({
-        text: process.env.TEXT
+async function notify() {
+    await fetch(process.env.SLACK_WEBHOOK, {
+        method: "POST",
+        body: JSON.stringify({
+            text: process.env.TEXT
+        })
     })
-})
+}
+notify()
